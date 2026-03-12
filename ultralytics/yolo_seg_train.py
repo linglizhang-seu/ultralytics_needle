a2d13e079998e806a9823a08f7a611da77c22720
UltralyticsAssistant <web@ultralytics.com>
2026-03-12 04:11:25 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/yolo_seg_train.py
+++ ultralytics/yolo_seg_train.py
@@ -1,5 +1,3 @@
-
-
 # import cv2
 # from pathlib import Path
 
@@ -19,51 +17,51 @@
 #         if im is None:
 #             print(f"无法读取 {img_path}, 跳过")
 #             continue
-        
+
 #         # 如果是灰度图，转换为 3 通道 RGB
 #         if len(im.shape) == 2 or im.shape[2] == 1:
 #             im = cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
-        
+
 #         # 保存到 images_rgb 对应子目录
 #         save_path = save_split_dir / img_path.name
 #         cv2.imwrite(str(save_path), im)
 #         print(f"已保存 {save_path}")
 
-import os
-import pathlib
-import onnx
-import cv2
-import albumentations as A
-from ultralytics import YOLO
 import torch
+
+from ultralytics import YOLO
+
 torch.cuda.empty_cache()
 # torch.cuda.empty_cache()
 torch.cuda.reset_peak_memory_stats()
+
+
 # os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 # os.chdir(os.path.dirname(__file__))  # 切
 def main():
-    weight_path = (r"E:\models\lightly_train\out\my_experiment_640\exported_last.pt")
+    weight_path = r"E:\models\lightly_train\out\my_experiment_640\exported_last.pt"
     # weight_path = r"E:\Human_Projects\yolov11\ultralytics-main\ultralytics\yolo11n-seg.pt"
 
-    model = YOLO(r"E:/Human_Projects/yolov11/ultralytics-main/ultralytics/cfg/models/11/yolo11-seg-wfu.yaml").load(weight_path)
-    result_train=model.train(
-            data="ultralytics/datasets/yolo.yaml",     # 你的分类数据集路径
-            epochs=400,
-            patience=100,
-            imgsz=224,
-            device="1",
-            workers=0,   
+    model = YOLO(r"E:/Human_Projects/yolov11/ultralytics-main/ultralytics/cfg/models/11/yolo11-seg-wfu.yaml").load(
+        weight_path
+    )
+    model.train(
+        data="ultralytics/datasets/yolo.yaml",  # 你的分类数据集路径
+        epochs=400,
+        patience=100,
+        imgsz=224,
+        device="1",
+        workers=0,
+        # 🔴 建议在 Windows 下设为 0 以避免多进程问         # 🔴 禁用混合精度训练，防止 NaN / Inf
+    )
 
-                          # 🔴 建议在 Windows 下设为 0 以避免多进程问         # 🔴 禁用混合精度训练，防止 NaN / Inf
-        )
-    
-    results=model.val(data="ultralytics/datasets/yolo.yaml")
+    results = model.val(data="ultralytics/datasets/yolo.yaml")
     # from ultralytics import YOLO
 
     # model = YOLO(r"E:/Human_Projects/yolov11/ultralytics-main/ultralytics/runs/segment/train7_my_experiment_6/weights/best.pt")
 
     # results = model.train(
-    #     data="ultralytics/datasets/yolo.yaml", 
+    #     data="ultralytics/datasets/yolo.yaml",
     #     epochs=150,
     #     freeze=3,           # 冻结前3层
     #     lr0=0.0005,         # 小学习率
@@ -71,10 +69,6 @@ def main():
     #     mosaic=0.0,         # 关闭强增强
     #     mixup=0.0
     # )
-    
-
-    
-
 
     print("F1 score:", results.box.f1)
     # print("F1 score curve:", results.box.f1_curve)
@@ -97,9 +91,5 @@ def main():
     model.export(format="onnx")
 
 
-    
-
-
 if __name__ == "__main__":
     main()
-   
